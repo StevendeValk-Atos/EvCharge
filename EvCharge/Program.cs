6a9ca4b0d437eed0a1df641237518219d105bf11
JochenHansoul <jochen.hansoul@outlook.be>
2023-05-10 17:16:43 +0200

    Added CORS to dotnet

--- EvCharge/Program.cs
+++ EvCharge/Program.cs
@@ -1,7 +1,18 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy  =>
+        {
+            policy.WithOrigins("https://localhost:44447");
+        }
+    );
+});
 
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -17,6 +28,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllerRoute(
     name: "default",
